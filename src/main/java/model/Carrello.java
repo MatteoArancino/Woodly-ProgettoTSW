400c07be89f98d690b281198a08255baf4dec435
MatteoArancino <m.arancino@studenti.unisa.it>
2026-05-21 14:47:50 +0200

    AGGIUNTA DELLA CLASSE CARRELLO (v1.0)

---
+++ src/main/java/model/Carrello.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrello {
+    private List<ItemCarrello> items;
+
+    public Carrello() {
+        this.items = new ArrayList<>();
+    }
+
+    public List<ItemCarrello> getItems() {
+        return items;
+    }
+
+    public void aggiungiProdotto(Prodotto p, int quantita) {
+        for (ItemCarrello item : items) {
+            if (item.getProdotto().getId() == p.getId()) {
+                item.incrementaQuantita(quantita);
+                return; // Prodotto trovato e aggiornato, usciamo dal metodo
+            }
+        }
+        items.add(new ItemCarrello(p, quantita));
+    }
+
+    public void rimuoviProdotto(int idProdotto) {
+        items.removeIf(item -> item.getProdotto().getId() == idProdotto);
+    }
+
+    public double getPrezzoTotaleCarrello() {
+        double totale = 0;
+        for (ItemCarrello item : items) {
+            totale += item.getPrezzoTotaleItem();
+        }
+        return totale;
+    }
+
+    public void svuota() {
+        items.clear();
+    }
+}
